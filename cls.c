48a2cc1884d3125c62bc1c5f4dfc4e3ffd9bafea
karrilaxminaidu24cse-stack <karrilaxminaidu.24.cse@anits.edu.in>
2026-07-13 09:29:51 +0530

    Create cls.c

---
+++ cls.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+   
+    char str[100];
+    scanf("%s",str);
+    int i=strlen(str);
+    printf("%d",i);
+        
+    return 0;
+}
